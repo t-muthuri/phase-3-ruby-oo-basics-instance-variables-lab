d9f9e9742dab3b5761969ec57cb1e75a79ef0aea
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-10-25 14:51:51 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+      end
+    #setting
+      def name
+        @this_dogs_name
+      end
+      #getting
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
